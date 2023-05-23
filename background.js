319276b8ae8f9679f14382927fea4a5727066604
asadeyy <asada.kanna.819@s.kyushu-u.ac.jp>
2023-05-24 02:16:30 +0900

    update delay

--- background.js
+++ background.js
@@ -4,12 +4,13 @@ window.onload = function () {
     .getElementsByClassName("card-text")[0]
     .getElementsByClassName("dropdown")[1]
     .getElementsByClassName("dropdown-menu");
+
   body[0].getElementsByClassName("dropdown-item")[1].click();
 
   setTimeout(function () {
     body[0].getElementsByClassName("dropdown-item")[1].click();
   }, 500);
-  //   setTimeout(function () {
-  //     body[0].getElementsByClassName("dropdown-item")[1].click();
-  //   }, 1000);
+  setTimeout(function () {
+    body[0].getElementsByClassName("dropdown-item")[1].click();
+  }, 2000);
 };
