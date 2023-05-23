17b873c588a996a7f9ffe7953b07a0ee15660599
asadeyy <asada.kanna.819@s.kyushu-u.ac.jp>
2023-05-24 00:41:00 +0900

    first commit

---
+++ background.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+  var body = document
+    .getElementsByClassName("card-body")[1]
+    .getElementsByClassName("card-text")[0]
+    .getElementsByClassName("dropdown")[1]
+    .getElementsByClassName("dropdown-menu");
+  body[0].getElementsByClassName("dropdown-item")[1].click();
+
+  setTimeout(function () {
+    body[0].getElementsByClassName("dropdown-item")[1].click();
+  }, 500);
+  //   setTimeout(function () {
+  //     body[0].getElementsByClassName("dropdown-item")[1].click();
+  //   }, 1000);
+};
